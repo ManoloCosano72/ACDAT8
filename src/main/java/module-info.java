54234639910b3e628ff3f04467b932c76beb1218
Manolo <cosanopalmamanuel@gmail.com>
2024-10-24 10:21:08 +0200

    Cuando va a dejar de fallar esto ostia

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,11 +4,11 @@ module com.github.ManoloCosano72 {
     requires java.xml.bind;
 
     opens com.github.ManoloCosano72 to javafx.fxml;
-    //opens com.github.ManoloCosano72.model.connection to java.xml.bind;
+    opens com.github.ManoloCosano72.utils to javafx.fxml;
+    opens com.github.ManoloCosano72.view to javafx.fxml;
+
 
     exports com.github.ManoloCosano72;
     exports com.github.ManoloCosano72.view;
-    opens com.github.ManoloCosano72.view to javafx.fxml;
     exports com.github.ManoloCosano72.utils;
-    opens com.github.ManoloCosano72.utils to javafx.fxml;
 }
